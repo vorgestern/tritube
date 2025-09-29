d929ea664a67881f6efa6df9f97a1e631fcf0a42
vorgestern <fisches@gmx.de>
2025-09-30 01:29:13 +0200

    Tritube/win32: simplified

--- src/tritube/win32/piper4.cpp
+++ src/tritube/win32/piper4.cpp
@@ -179,51 +179,50 @@ rc_out_err tritube::piper4_roe(fspath&fullpath, string_view args)
 
     CloseHandle(ph.write_to_stdin);
 
+    struct channel
+    {
+        HANDLE read_from_child;
+        OVERLAPPED ol;
+        vector<string>merk {};
+        bool closed {false};
+        char buffer[1024];
+        DWORD nr {0};
+        void read()
+        {
+            if (!ReadFile(read_from_child, buffer, sizeof buffer, &nr, &ol))
+            {
+                switch (GetLastError())
+                {
+                    case ERROR_IO_PENDING: break;
+                    default:
+                    case ERROR_BROKEN_PIPE: closed=true; break;
+                }
+            }
+        }
+    } out={ph.read_from_stdout, ph.olout},
+      err={ph.read_from_stderr, ph.olerr};
+
     int rcx=-1;
-    vector<string>merk_out, merk_err;
-    bool closed_stderr=false, closed_stdout=false;
-    char ccout[1024]="", ccerr[1024]="";
-    DWORD nrout=0, nrerr=0;
     bool fterm=false;
 
-    if (!ReadFile(ph.read_from_stdout, ccout, sizeof ccout, &nrout, &ph.olout)) closed_stdout=true;
-    if (!ReadFile(ph.read_from_stderr, ccerr, sizeof ccerr, &nrerr, &ph.olerr)) closed_stderr=true;
+    out.read();
+    err.read();
 
-    auto handle_stderr=[&nrerr,&ccerr,&closed_stderr,&ph,&merk_err]()
+    auto handle_stderr=[&err]()
     {
-        if (nrerr>0)
-        {
-            merk_err.emplace_back(ccerr);
-            if (!ReadFile(ph.read_from_stderr, ccerr, sizeof ccerr, &nrerr, &ph.olerr))
-            {
-                switch (GetLastError())
-                {
-                    case ERROR_IO_PENDING: break;
-                    default:
-                    case ERROR_BROKEN_PIPE: closed_stderr=true; break;
-                }
-            }
-        }
+        if (err.nr<=0) return;
+        err.merk.emplace_back(err.buffer, err.nr);
+        err.read();
     };
 
-    auto handle_stdout=[&nrout,&ccout,&closed_stdout,&ph,&merk_out]()
+    auto handle_stdout=[&out]()
     {
-        if (nrout>0)
-        {
-            merk_out.emplace_back(ccout);
-            if (!ReadFile(ph.read_from_stdout, ccout, sizeof ccout, &nrout, &ph.olout))
-            {
-                switch (GetLastError())
-                {
-                    case ERROR_IO_PENDING: break;
-                    default:
-                    case ERROR_BROKEN_PIPE: closed_stdout=true; break;
-                }
-            }
-        }
+        if (out.nr<=0) return;
+        out.merk.emplace_back(out.buffer, out.nr);
+        out.read();
     };
 
-    auto handle_process=[&fterm, &rcx, &closed_stderr, &closed_stdout, ph]()
+    auto handle_process=[&fterm, &rcx, ph]()
     {
         unsigned long exitcode=STILL_ACTIVE;
         if (GetExitCodeProcess(ph.process, &exitcode))
@@ -255,14 +254,14 @@ rc_out_err tritube::piper4_roe(fspath&fullpath, string_view args)
         else if (r>=WAIT_ABANDONED_0 && r<WAIT_ABANDONED_0+numobj) break; // TSNH
         else if (r==WAIT_TIMEOUT)
         {
-            if (closed_stdout && closed_stderr) fterm=true;
+            if (out.closed && err.closed) fterm=true;
             break;
         }
         else if (r==WAIT_FAILED){ fterm=true; break; } // TSNH
         else                    { fterm=true; break; } // TSNH
     }
     string so, se;
-    for (auto&m: merk_out) so.append(m);
-    for (auto&m: merk_err) se.append(m);
+    for (auto&m: out.merk) so.append(m);
+    for (auto&m: err.merk) se.append(m);
     return {rcx, so, se};
 }
