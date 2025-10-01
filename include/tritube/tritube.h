c9c1fcf9cfae9ac310c71d74a04ddb3bee7b2555
vorgestern <fisches@gmx.de>
2025-10-01 21:10:41 +0200

    Tritube: Simplified
    - No more templates, underlying implementation functions are called directly.
      The name suffixes are at least as readable as the templated calls.

--- include/tritube/tritube.h
+++ include/tritube/tritube.h
@@ -19,33 +19,4 @@ namespace tritube
         std::function<void(const std::string&)>process_stdout, std::function<void(const std::string&)>process_stderr);
 
     std::optional<std::filesystem::path> applpath(xfind, std::string_view exec_name);
-
-    inline std::string exec_sync1(std::filesystem::path&exec, std::string_view args)
-    {
-        return piper4_o(exec, args);
-    }
-
-    template<typename resulttype, xfind X>resulttype exec_sync(std::string_view exec_name, std::string_view args)
-    {
-        if (auto fullpath=applpath<X>(exec_name); fullpath.has_value())
-        {
-            std::filesystem::path P=fullpath.value();
-            return exec_sync1(P, args);
-        }
-        else return {};
-    }
-
-    template<typename resulttype> resulttype exec_simple(std::filesystem::path&exec, std::string_view args);
-    template<> inline std::string exec_simple<std::string>(std::filesystem::path&exec, std::string_view args)
-    {
-        return piper4_o(exec, args);
-    }
-    template<> inline rc_out_err exec_simple<rc_out_err>(std::filesystem::path&exec, std::string_view args)
-    {
-        return piper4_roe(exec, args);
-    }
-    template<> inline rc_Out_Err exec_simple<rc_Out_Err>(std::filesystem::path&exec, std::string_view args)
-    {
-        return piper4_ROE(exec, args);
-    }
 }
