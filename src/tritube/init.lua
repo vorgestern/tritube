5998f27eb9ef12b5897270436cbae837337dc388
vorgestern <fisches@gmx.de>
2025-10-03 22:51:17 +0200

    Tritube: Intermediate (applpath)
    - Provide separate implementations of applpath for win32 and linux.
      (No difference yet.)

--- src/tritube/init.lua
+++ src/tritube/init.lua
@@ -3,20 +3,19 @@ local Workspace=require "Workspace"
 
 project "tritube"
 kind "StaticLib"
-files {"*.cpp", "../../include/tritube/*.h"}
+files "../../include/tritube/*.h"
 
 includedirs {"../../include"}
 
 if Workspace.system=="windows" then
     files {"win32/*.cpp", "win32/*.h"}
     vpaths {
-        [""]={"*.cpp", "../../include/tritube/*.h"},
+        [""]={"../../include/tritube/*.h"},
         win32={"win32/*.cpp", "win32/*.h"}
     }
-	disablewarnings {4100}
+    disablewarnings {4100}
 elseif Workspace.system=="linux" then
     files {"linux/*.cpp", "linux/*.h"}
-	files "../forkpipes*"
 end
 
 optimize "on"
--- src/tritube/win32/winhelper.cpp
+++ src/tritube/win32/winhelper.cpp
@@ -141,3 +141,52 @@ int entertain(HANDLE hprocess, inputchannel_async&out, function<void()>handle_ou
     err.closehandles();
     return rcx;
 }
+
+#include <iostream>
+#include <tritube/tritube.h>
+
+using namespace tritube;
+
+static vector<fspath> pathdirectories()
+{
+    vector<fspath>result;
+    const string P=getenv("PATH");
+    size_t pos=0;
+    while (true)
+    {
+        auto s=P.find_first_of(";:", pos);
+        if (s!=P.npos){ result.push_back(P.substr(pos, s-pos)); pos=s+1; }
+        else{ result.push_back(P.substr(pos)); break; }
+    }
+    return result;
+}
+
+// ============================================================================
+
+optional<fspath> tritube::applpath(xfind f, string_view appp)
+{
+    fspath appl(appp);
+    switch (f)
+    {
+        case xfdirect:
+        {
+            if (appl.is_absolute()) return filesystem::exists(appl)?appl:optional<fspath>();
+            error_code ec;
+            auto here=filesystem::current_path(ec);
+            if (ec) return {};
+            auto versuch=here / appl;
+            return filesystem::exists(versuch)?versuch:optional<fspath>();
+        }
+        case xfpath:
+        {
+            auto Paths=pathdirectories();
+            for (auto&k: Paths)
+            {
+                const auto versuch=k / appl;
+                if (filesystem::exists(versuch)) return versuch;
+            }
+            return {};
+        }
+        default: return {};
+    }
+}
