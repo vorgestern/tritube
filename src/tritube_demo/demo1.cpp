5063537e6c805b7ecdabe193fe63eb754d40c136
vorgestern <fisches@gmx.de>
2025-10-06 10:55:38 +0200

    Tritube_demo: extended
    - Added usecases with parsable output for unittests.

--- src/tritube_demo/demo1.cpp
+++ src/tritube_demo/demo1.cpp
@@ -5,6 +5,29 @@
 using namespace std;
 using namespace tritube;
 
+static size_t numlines(const string&X)
+{
+    if (X.empty()) return 0;
+    size_t num=0;
+    size_t p=0;
+    while (p!=X.npos)
+    {
+        auto neu=X.find_first_of('\n', p);
+        if (neu!=X.npos)
+        {
+            ++num;
+            p=neu+1;
+        }
+        else if (p<X.size())
+        {
+            ++num;
+            p=X.npos;
+        }
+        else p=X.npos;
+    }
+    return num;
+}
+
 static ostream&operator<<(ostream&out, const vector<string>&args)
 {
     unsigned j=0;
@@ -25,8 +48,9 @@ void demo_default()
 
 int main(int argc, char*argv[])
 {
-    enum {print, out, roe, roev, linewise} usecase=out;
+    enum {print, out, out_raw, roe, roe_raw, roev, roev_raw, linewise, linewise_raw} usecase=out;
     xfind search=xfpath;
+    bool output_raw=false;
     auto a=1;
     for (a=1; a<argc; ++a)
     {
@@ -57,7 +81,16 @@ int main(int argc, char*argv[])
                 exit(1);
             }
         }
+        else if (arg=="--raw") output_raw=true;
     }
+    if (output_raw) switch (usecase)
+    {
+        case out: usecase=out_raw; break;
+        case roe: usecase=roe_raw; break;
+        case roev: usecase=roev_raw; break;
+        case linewise: usecase=linewise_raw; break;
+    }
+
     string appl="git.exe";
     vector<string> args={"--version"};
     if (a<argc)
@@ -79,6 +112,13 @@ int main(int argc, char*argv[])
                 exit(Text.size()>0?0:1);
                 break;
             }
+            case out_raw:
+            {
+                auto Text=piper_o(p.value(), args);
+                if (Text.size()>0) cout<<Text;
+                exit(Text.size()>0?0:1);
+                break;
+            }
             case roe:
             {
                 auto [rc, out, err]=piper_roe(p.value(), args);
@@ -86,6 +126,16 @@ int main(int argc, char*argv[])
                     <<"\n== stdout ======================================\n"<<out
                     <<"\n== stderr ======================================\n"<<err
                     <<"\n================================================\n";
+                exit(rc==0?0:1);
+            }
+            case roe_raw:
+            {
+                auto [rc, out, err]=piper_roe(p.value(), args);
+                cout<<rc<<" "<<numlines(out)<<" "<<numlines(err)<<"\n";
+                cout<<out;
+                if (out.back()!='\n') cout<<"\n";
+                cout<<err;
+                if (err.back()!='\n') cout<<"\n";
                 exit(rc==0?0:1);
             }
             case roev:
@@ -99,6 +149,14 @@ int main(int argc, char*argv[])
                 cout<<"================================================\n";
                 exit(rc==0?0:1);
             }
+            case roev_raw:
+            {
+                auto [rc, Out, Err]=piper_roev(p.value(), args);
+                cout<<rc<<" "<<Out.size()<<" "<<Err.size()<<"\n";
+                for (auto&k: Out) cout<<k<<"\n";
+                for (auto&k: Err) cout<<k<<"\n";
+                exit(rc==0?0:1);
+            }
             case linewise:
             {
                 auto rc=piper_linewise(p.value(), args,
@@ -106,6 +164,17 @@ int main(int argc, char*argv[])
                     [](const string&X){ cout<<"?? "<<X<<" ??\n"; });
                 cout<<  "== return code =================================\n"<<rc
                     <<"\n================================================\n";
+                exit(rc==0?0:1);
+            }
+            case linewise_raw:
+            {
+                vector<string> Out, Err;
+                auto rc=piper_linewise(p.value(), args,
+                    [&Out](const string&X){ Out.push_back("out: "+X); },
+                    [&Err](const string&X){ Err.push_back("err: "+X); });
+                cout<<rc<<" "<<Out.size()<<" "<<Err.size()<<"\n";
+                for (auto&k: Out) cout<<k<<"\n";
+                for (auto&k: Err) cout<<k<<"\n";
                 exit(rc==0?0:1);
             }
         }
